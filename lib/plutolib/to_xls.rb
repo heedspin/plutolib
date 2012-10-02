6162b45f2caece7935ae6b04861ec8a070b01817
Tim Harrison <heedspin@gmail.com>
2012-10-02 10:22:14 -0400

    Fork to_xls from xls_report.

--- lib/plutolib/to_xls.rb
+++ lib/plutolib/to_xls.rb
@@ -1,7 +1,6 @@
 require 'spreadsheet'
 
-# Deprecated: Moving to ToXls
-module Plutolib::XlsReport
+module Plutolib::ToXls
   class Field
     attr_reader :column_header, :number_format
     def initialize(column_header, number_format=nil, &value_block)
@@ -14,26 +13,30 @@ module Plutolib::XlsReport
     end
   end
   
-  def fields
-    if @fields.nil?
-      @fields = []
-      self.initialize_fields
-    end
-    @fields
+  def xls_initialize
+    raise 'Implement me!!!'
   end
   
-  def add_field(field)
-    self.fields.push(field)
+  def xls_fields
+    if @xls_fields.nil?
+      @xls_fields = []
+      self.xls_initialize
+    end
+    @xls_fields
+  end
+  
+  def xls_field(column_header, number_format=nil, &value_block)
+    self.xls_fields.push Plutolib::ToXls::Field.new(column_header, number_format, &value_block)
   end
   
   def to_xls
     book = Spreadsheet::Workbook.new
     sheet = book.create_worksheet
-    sheet.name = self.sheet_name
-    column_formats = get_column_formats(self.fields, sheet)
+    sheet.name = self.xls_sheet_name
+    column_formats = xls_column_formats(self.xls_fields, sheet)
     self.all_data.each do |data_object|
       sheet_row = sheet.row(sheet.last_row_index+1)
-      row_data = self.fields.map { |field| 
+      row_data = self.xls_fields.map { |field| 
         field.value_for(data_object) 
       }
       sheet_row.push *row_data
@@ -48,31 +51,50 @@ module Plutolib::XlsReport
     s.string
   end
   
+  def xls_filename
+    self.class.name.demodulize.underscore
+  end
+  
+  def xls_sheet_name
+    self.class.name.demodulize.humanize
+  end
+  
+  def xls_time_format
+    @xls_time_format ||= Spreadsheet::Format.new(:number_format => 'h:mm:ss AM/PM')
+  end
+  
+  def xls_text_format
+    @xls_text_format ||= Spreadsheet::Format.new
+  end
+  
+  def xls_date_format
+    @xls_date_format ||= Spreadsheet::Format.new(:number_format => 'mm/dd/yyyy')
+  end
+  
+  def xls_header_format
+    @xls_header_format ||= Spreadsheet::Format.new :weight => :bold, :size => 12
+  end  
+
   protected
 
-    def get_column_formats(fields, sheet)
-      @time_format   ||= Spreadsheet::Format.new(:number_format => 'h:mm:ss AM/PM')
-      @text_format   ||= Spreadsheet::Format.new
-      @date_format   ||= Spreadsheet::Format.new(:number_format => 'mm/dd/yyyy')
-      @header_format ||= Spreadsheet::Format.new :weight => :bold, :size => 12
-
+    def xls_column_formats(fields, sheet)
       default_column_formats = {}
       for x in 0..fields.size-1
         field = fields[x]
         sheet.row(0).push field.column_header
-        sheet.row(0).set_format(x, @header_format)
+        sheet.row(0).set_format(x, self.xls_header_format)
         if field.number_format.present?
           default_column_formats[x] = field.number_format
         else
           case field.column_header
           when /Time/
             sheet.column(x).width = 11
-            default_column_formats[x] = @time_format
+            default_column_formats[x] = self.xls_time_format
           when /Date/
             sheet.column(x).width = 11
-            default_column_formats[x] = @date_format
+            default_column_formats[x] = self.xls_date_format
           else
-            default_column_formats[x] = @text_format
+            default_column_formats[x] = self.xls_text_format
           end
         end
       end
@@ -89,7 +111,7 @@ module Plutolib::XlsReport
     # 195.chr => \303 => \xC3  ==> bang
     # 226.chr => \342 => \xE2
     EVIL_CHARACTERS = [153, 160, 162, 194, 195, 226]
-    def clean_value(txt)
+    def xls_clean(txt)
       return txt unless txt.is_a?(String)
 
       # Evil curly quote replace
--- lib/plutolib/version.rb
+++ lib/plutolib/version.rb
@@ -1,3 +1,3 @@
 module Plutolib
-  VERSION='0.2.1'
+  VERSION='0.2.2'
 end
