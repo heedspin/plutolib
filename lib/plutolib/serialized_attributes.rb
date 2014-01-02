2dd03d7ea3424b3ae1fcf4ecdb054a0f44cce285
Tim Harrison <heedspin@gmail.com>
2014-01-02 18:55:13 -0500

    Play with production loading issue on ruby 1.9.3.

--- lib/plutolib/serialized_attributes.rb
+++ lib/plutolib/serialized_attributes.rb
@@ -1,48 +1,50 @@
-module Plutolib::SerializedAttributes
-  def self.included(base)
-    class << base
-      def serialized_attribute(key, args=nil)
-        args ||= {}
-        storage_column = args[:data] || 'data'
-        deserialize = args[:deserialize] || args[:des]
-        default = args.member?(:default) ? args[:default] : 0
-        unless self.method_defined?(storage_column)
-          self.class_eval <<-RUBY
-          def #{storage_column}
-            if @#{storage_column}.nil?
-              if x = super
-                @#{storage_column} = x.is_a?(String) ? ActiveSupport::JSON.decode(x) : x
-              else
-                @#{storage_column} = Hash.new(#{default})
+module Plutolib
+  module SerializedAttributes
+    def self.included(base)
+      class << base
+        def serialized_attribute(key, args=nil)
+          args ||= {}
+          storage_column = args[:data] || 'data'
+          deserialize = args[:deserialize] || args[:des]
+          default = args.member?(:default) ? args[:default] : 0
+          unless self.method_defined?(storage_column)
+            self.class_eval <<-RUBY
+            def #{storage_column}
+              if @#{storage_column}.nil?
+                if x = super
+                  @#{storage_column} = x.is_a?(String) ? ActiveSupport::JSON.decode(x) : x
+                else
+                  @#{storage_column} = Hash.new(#{default})
+                end
               end
+              @#{storage_column}
+            end        
+            protected
+              before_save :serialize_#{storage_column}
+              def serialize_#{storage_column}
+                self.#{storage_column} = self.#{storage_column}.to_json
+              end
+            RUBY
+          end
+          if deserialize
+            self.class_eval <<-RUBY
+            def #{key}
+              self.#{storage_column}['#{key}'].try(:send, '#{deserialize.to_s}')
             end
-            @#{storage_column}
-          end        
-          protected
-            before_save :serialize_#{storage_column}
-            def serialize_#{storage_column}
-              self.#{storage_column} = self.#{storage_column}.to_json
+            RUBY
+          else
+            self.class_eval <<-RUBY
+            def #{key}
+              self.#{storage_column}['#{key}']
             end
-          RUBY
-        end
-        if deserialize
+            RUBY
+          end
           self.class_eval <<-RUBY
-          def #{key}
-            self.#{storage_column}['#{key}'].try(:send, '#{deserialize.to_s}')
+          def #{key}=(val)
+            self.#{storage_column}['#{key}'] = val
           end
           RUBY
-        else
-          self.class_eval <<-RUBY
-          def #{key}
-            self.#{storage_column}['#{key}']
-          end
-          RUBY
         end
-        self.class_eval <<-RUBY
-        def #{key}=(val)
-          self.#{storage_column}['#{key}'] = val
-        end
-        RUBY
       end
     end
   end
